52cbf9e5f65f52b8ea6f50484ecf0a49393e7853
iLtc <i@iltc.io>
2015-09-26 21:17:47 +0000

    Add Xshow images

--- xshow.inc.php
+++ xshow.inc.php
@@ -5,29 +5,35 @@ $result = array();
 
 $wzkk_xshow = $_G['cache']['plugin']['wzkk_xshow'];
 
-$datapic = array();
-$fids = $wzkk_xshow['only_pic'] ? 'AND t.fid IN('.$wzkk_xshow['only_pic'].')' : '';
-$PicNums = $wzkk_xshow['pic_schnum'];
-/**ljm修改图片显示，禁止显示非显版块的图片*/	
-//echo $wzkk_xshow['not_show'];
-$notshow = $wzkk_xshow['not_show'] ? 'AND t.fid not IN('.$wzkk_xshow['not_show'].')' : '';//不显示的版块  all
-//插入到下面的sql中
-/**end ljm*/
-if ($wzkk_xshow['pic_transfer'] == 1) { 
-	$orderby = 'tid'; 
-} else if ($wzkk_xshow['pic_transfer'] == 2) { 
-	$orderby = 'tid'; 
-} else { 
-	$orderby = 'rand'; 
+switch($_G['gp_action']){
+	case 'image':
+		$datapic = array();
+		$fids = $wzkk_xshow['only_pic'] ? 'AND t.fid IN('.$wzkk_xshow['only_pic'].')' : '';
+		$PicNums = $wzkk_xshow['pic_schnum'];
+		/**ljm修改图片显示，禁止显示非显版块的图片*/	
+		//echo $wzkk_xshow['not_show'];
+		$notshow = $wzkk_xshow['not_show'] ? 'AND t.fid not IN('.$wzkk_xshow['not_show'].')' : '';//不显示的版块  all
+		//插入到下面的sql中
+		/**end ljm*/
+		if ($wzkk_xshow['pic_transfer'] == 1) { 
+			$orderby = 'tid'; 
+		} else if ($wzkk_xshow['pic_transfer'] == 2) { 
+			$orderby = 'tid'; 
+		} else { 
+			$orderby = 'rand'; 
+		}
+		$orderby = $orderby != 'rand' ? 'attach.'.$orderby : 'rand()';
+		$query = DB::query("SELECT attach.attachment,t.tid, t.fid, t.subject FROM ".DB::table('forum_threadimage')." attach INNER JOIN ".DB::table('forum_thread')." t ON t.tid=attach.tid WHERE t.isgroup=0 AND t.displayorder>=0 $fids $notshow GROUP BY attach.tid ORDER BY $orderby DESC LIMIT 0, ".$PicNums);
+		while($pic = DB::fetch($query)) {
+			$pics['src'] = $_G['setting']['attachurl'].'forum/'.$pic['attachment'];
+			$pics['tid'] = $pic['tid'];
+			$pics['pictexts'] = iconv("GBK", "UTF-8", str_replace('\'', ' ',$pic['subject']));
+			$datapic[] = $pics;
+		}
+		$result['images'] = $datapic;
+		showResult($result, 'success');
+		break;
+	
+	default:
+		showError('Missing Parameter');
 }
-$orderby = $orderby != 'rand' ? 'attach.'.$orderby : 'rand()';
-$query = DB::query("SELECT attach.attachment,t.tid, t.fid, t.subject FROM ".DB::table('forum_threadimage')." attach INNER JOIN ".DB::table('forum_thread')." t ON t.tid=attach.tid WHERE t.isgroup=0 AND t.displayorder>=0 $fids $notshow GROUP BY attach.tid ORDER BY $orderby DESC LIMIT 0, ".$PicNums);
-while($pic = DB::fetch($query)) {
-	$pics['picpics'] = $_G['setting']['attachurl'].'forum/'.$pic['attachment'];
-	$pics['piclinks'] = 'forum.php?mod=viewthread%26tid='.$pic['tid'];
-	$pics['pictexts'] = str_replace('\'', ' ',$pic['subject']);
-	$pics['attaid'] = $pic['aid'];
-	$datapic[] = $pics;
-}
-
-var_dump("SELECT attach.attachment,t.tid, t.fid, t.subject FROM ".DB::table('forum_threadimage')." attach INNER JOIN ".DB::table('forum_thread')." t ON t.tid=attach.tid WHERE t.isgroup=0 AND t.displayorder>=0 $fids $notshow GROUP BY attach.tid ORDER BY $orderby DESC LIMIT 0, ".$PicNums, $datapic);
